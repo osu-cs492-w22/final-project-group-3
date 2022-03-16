a2afe33af270f7e1bfec6bc7891fc7db0d71857d
kiefk <kiefk@oregonstate.edu>
2022-03-16 09:48:35 -0700

    Updated temp api key

--- app/src/main/java/com/example/banishthem/RiotService.kt
+++ app/src/main/java/com/example/banishthem/RiotService.kt
@@ -8,7 +8,7 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 //API Key
-private const val API_KEY = "RGAPI-0831445a-a8e4-453b-bdff-651ea2060e3f"
+private const val API_KEY = "RGAPI-0c0ab32a-7a1d-48d7-ade4-0697174c7067"
 
 interface RiotService {
     @GET("/lol/summoner/v4/summoners/by-name/{name}?api_key=${API_KEY}")
