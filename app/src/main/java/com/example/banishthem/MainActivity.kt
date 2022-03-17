11551fc2636951ec6c76a33c0fdeff50d9cdec35
kwakd <kwakd.oregonstate.edu>
2022-03-16 20:38:01 -0700

    settings almost done

--- app/src/main/java/com/example/banishthem/MainActivity.kt
+++ app/src/main/java/com/example/banishthem/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.banishthem
 
+import android.content.Intent
+import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.Toolbar
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+
+import android.util.Log
+import androidx.preference.PreferenceManager
 
 val summonersArray = arrayOf(
     "Summoner1",
@@ -37,7 +44,39 @@ class MainActivity : AppCompatActivity() {
             tab.text = summonersArray[position]
         }.attach()
 
-
-
+        mySettings()
     }
+
+    private fun mySettings(){
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+        val switch = prefs.getBoolean("dlMode_settings", false)
+
+        if(switch){
+            Log.i("switch is on", "SWITCH ON")
+        }
+        else{
+            Log.i("switch is off", "SWITCH OFF")
+        }
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.settings_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.action_settings ->{
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
 }
